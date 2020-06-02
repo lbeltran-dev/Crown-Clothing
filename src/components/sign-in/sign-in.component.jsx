7d3b2f6f231a470aeb22f899e2b02fed42d5a274
lbeltran <lbeltran.dev@gmail.com>
2020-06-01 22:18:12 -0600

    Added Redux, and implemented userReducer and userActions to header and app components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -17,16 +17,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = async event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
     const { email, password } = this.state;
 
     try {
-        await auth.signInWithEmailAndPassword(email, password);
-        this.setState({ email: '', password: ''})
-    } catch(error) {
-        console.log(error);
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
     }
     this.setState({ email: "", password: "" });
   };
@@ -62,7 +62,11 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit"> Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </div>
